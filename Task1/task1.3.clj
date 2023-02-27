189f756c3a577450dc808ef8ad706035f92f777c
pishchev <71860222+pishchev@users.noreply.github.com>
2023-02-27 20:41:30 +0700

    Create task1.3.clj
    1.3

---
+++ Task1/task1.3.clj
@@ -0,0 +1,24 @@
+(defn map_ [func input]
+    (apply list 
+        (reduce 
+            (fn [acc x] (conj acc (func x))) 
+            [] 
+            input
+        )
+    )
+)
+
+(println (map_ inc (list 1 2 3 4)))
+
+
+(defn filter_ [func input]
+    (apply list 
+        (reduce 
+            (fn [acc x] (if (func x) (conj acc x) acc))
+            []
+            input
+        )
+    )
+)
+
+(println (filter_ (fn [x] (not= 0 (mod x 2))) (range 0 10)))
