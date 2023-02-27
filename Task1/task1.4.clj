5396d17281e2e0c0bb0448a1d1c218bb10f82757
pishchev <71860222+pishchev@users.noreply.github.com>
2023-02-27 21:07:10 +0700

    1.4

---
+++ Task1/task1.4.clj
@@ -0,0 +1,23 @@
+(defn addToWord [baseStr alpList]
+    (map
+        (fn [sym] (str baseStr (first sym)))
+        (filter (fn [sym] (not= (str(last baseStr)) sym)) alpList )
+    )
+)
+
+(defn addToWordList[restAlpList alpList]
+    (reduce concat
+        (map (fn [sym] (addToWord sym alpList)) restAlpList)
+    )
+)
+
+(defn combine [alpList curIterIdx]
+  (if (> curIterIdx 1)
+    (addToWordList (combine alpList (dec curIterIdx)) alpList)
+    alpList
+  )
+)
+
+(println (combine `("a" "b" "c") 1))
+(println (combine `("a" "b" "c") 2))
+(println (combine `("a" "b" "c") 3))
