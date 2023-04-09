f9ee2acfd4957237ffe224f86c2a0863a5ffac78
ivan <pishevv17@mail.ru>
2023-04-09 18:24:13 +0700

    i

--- Task3/task3.1.clj
+++ Task3/task3.1.clj
@@ -3,6 +3,7 @@
 
 (def prec 0.1)
 (defn f_sleep [x] (do (Thread/sleep 1) x))
+(defn fx_x3 [x] (/(* (* x x) x) 3))
 (defn fx_x2 [x] (* x x))
 
 (defn trapezoid [f x0 x1]
@@ -41,6 +42,17 @@
     )
   )
 )
+
+; тестирование
+(ns Task.2.1 (:use clojure.test) (:use integration))
+(defn abs [n] (max n (- n)))
+(defn is_close [x0 x1] ( < (abs(- x0 x1)) prec))
+
+(deftest functions-test
+  (is (is_close ((integrate fx_x3) 3) 6.7575))
+  (is (is_close ((integrate fx_x2) 5) 41.675))
+)
+(run-tests 'Task.2.1)
 
 (println "No meme")
 (time ((integrate f_sleep) 11.3))
